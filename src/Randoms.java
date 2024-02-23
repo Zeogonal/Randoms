a537177b415398adb8665d1efb760e22632c648e
Zeogonal <minzia@mail.ru>
2024-02-23 22:51:51 +0300

    Initial commit

---
+++ src/Randoms.java
@@ -0,0 +1,32 @@
+import java.util.Iterator;
+import java.util.Random;
+
+/**
+ * @author Stanislav Rakitov
+ */
+public class Randoms implements Iterable<Integer> {
+  protected Random random;
+  private final int min;
+  private final int max;
+
+  public Randoms(int min, int max) {
+    random = new Random();
+    this.min = min;
+    this.max = max;
+  }
+
+  @Override
+  public Iterator<Integer> iterator() {
+    return new Iterator<>() {
+      @Override
+      public boolean hasNext() {
+        return true;
+      }
+
+      @Override
+      public Integer next() {
+        return random.nextInt(max - min + 1) + min;
+      }
+    };
+  }
+}
